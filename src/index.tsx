30b4ae9713d25842f828e70c0e051a631f254046
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-03-19 16:51:47 +0200

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-type HeaderProps = {
-    title: string
-}
-
-const Header = ({ title }: HeaderProps) => {
-    return <h1>Helow {title}</h1>
-}
-
-const App = () => {
-    return (
-        <>
-            <Header title="React" />
-            <Header title="JS" />
-        </>
-    )
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
